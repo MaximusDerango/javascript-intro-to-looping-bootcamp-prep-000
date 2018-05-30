8e6d6da23644a75458d3fdbabed862a268686b8f
Maxim Dalton <maximcdalton@gmail.com>
2018-05-30 20:12:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 var forLoop = function(array) {
   for (let i = 0; i < 25; i++) {
-    array.push(`I am ${} strange loop${i === 0 ? '' : 's'}`);
+    array.push(`I am ${} strange loop${i === 0 ? '' : 's'}`)
   }
   return array
 }
